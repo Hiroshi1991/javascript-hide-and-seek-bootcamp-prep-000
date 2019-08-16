b7ba142ba0a6dbc1932fbc211ecf9e7309b362a0
Hiroshi S <sipahioglu_7@hotmail.com>
2019-08-16 15:00:26 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,21 +1,56 @@
-function getFirstSelector(selelctor){
-  document.querySelector(selector)
-  }
-
- function nestedTarget(){
-  document.querySelector('#nested .target')
+function getFirstSelector(selector) {
+  return document.querySelector(selector)
 }
 
- function increaseRankBy(n){
-var list = document.querySelectorAll('ul.ranked-list li')
-for (let i = 0; i < list.length;i++) {
-  list[i].innerHTML= parseInt(list[i].innerHTML, 10) + n;
+ function nestedTarget() {
+    // var tmpParent = document.getElementsByClassName('nested')
+    // tmpParent.
+    // var tmpChild = tmpParent.children
+//  find(tmpChild,'target')
+    var tmp = document.getElementsByClassName('target')[0]
+    return tmp
 }
+
+ function increaseRankBy(n) {
+    var lis = document.getElementById('app').querySelectorAll('ul.ranked-list li')
+    for (var i=0;i<lis.length;i++) {
+      lis[i].innerHTML = parseInt(lis[i].innerHTML) + n
+    }
 }
 
  function deepestChild() {
-  const list = document.getElementById('app').querySelectorAll('ul.ranked-list li')
-  for (let i=0, 1=list.length; i<1; i++) {
-    list[i].innerHTML = (i+1).toString()
+    var cur = document.getElementById('grand-node')
+    var i = 0
+    while (cur.firstElementChild) {
+      i = i + 1
+      cur = cur.firstElementChild
+    }
+    return cur
+}
+
+
+ function criteriaFN(criteria) {
+  if (criteria == "target") {
+    return true
   }
-  
+  else {
+    return false
+  }
+}
+
+ function find(array,criteriaFN) {
+  let current = array
+  let next = []
+  while (current) {
+    if (criteriaFN(current)){
+      return current
+    }
+    if (Array.isArray(current)) {
+      for (let i = 0; i < current.length; i++) {
+      next.push(current[i])
+      }
+    }
+    current =next.shift()
+  }
+  return null
+}
