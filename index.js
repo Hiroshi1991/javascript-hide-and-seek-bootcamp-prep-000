e38a25f5334db5e446aa4b4e1b19c8927ae0287f
Hiroshi S <sipahioglu_7@hotmail.com>
2019-08-16 14:42:11 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
+function getFirstSelector(selector){
+  var test = document.querySelector(selector);
+  //console.log(test);
+  return document.querySelector(selector);
+}
 
+function nestedTarget() {
+  return document.querySelector('#nested .target');
+}
+
+function increaseRankBy(n) {
+  var elements = document.querySelectorAll('ul.ranked-list li');
+
+  for (var i = 0; i < elements.length; i++) {
+    elements[i].innerHTML = parseInt(elements[i].innerHTML) + parseInt(n);
+  }
+}
+
+function deepestChild() {
+  var parent = document.getElementById('grand-node');
+}
+
+var child = "";
+
+function deepestChild() {
+  var parent = document.getElementById('grand-node');
+  
+}
