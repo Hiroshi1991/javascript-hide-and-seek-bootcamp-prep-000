305154df3c2e449d8137464b3c1692aa4468a5aa
Hiroshi S <sipahioglu_7@hotmail.com>
2019-08-16 14:58:35 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,26 +1,20 @@
-function getFirstSelector(selector){
-  return document.querySelector(selector)
-
- }
-
- function nestedTarget(){
-  return document.getElementById('nested').querySelector('div.target')
-
- }
-function increaseRankBy(n){
-  var match = document.querySelectorAll('ranked-list li')
-  for (let i=0; i<match.length;i++){
-    match[i].innerHTML = parseInt(match[i].innerHTML)+n;
+function getFirstSelector(selelctor){
+  document.querySelector(selector)
   }
 
- }
-
- function deepestChild(){
-  var deep = document.getElementById('grand-node').querySelectorAll('div')
-  return deep[deep.length-1]
-
- }
-
-function test(){
+ function nestedTarget(){
+  document.querySelector('#nested .target')
 }
-  var match = document.querySelectorAll('#grand-node')
+
+ function increaseRankBy(n){
+var list = document.querySelectorAll('ul.ranked-list li')
+for (let i = 0; i < list.length;i++{
+  list[i].innerHTML= parseInt(list[i].innerHTML, 10) + n;
+  }
+}
+
+ function deepestChild() {
+  const list = document.getElementById('app').querySelectorAll('ul.ranked-list li')
+  for (let i=0, 1=list.length; i<1; i++) {
+    list[i].innerHTML = (i+1).toString()
+  }
